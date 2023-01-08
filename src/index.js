96162966204b6bf17239b84d29e6992b3bdd7fbf
Brian Yoder <brianyoder@gmail.com>
2023-01-08 13:29:52 -0800

    add initial html
    Co-authored-by: chlobear8 <chlobear8@users.noreply.github.com>
    Co-authored-by: nsncowan <nsncowan@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -2,3 +2,15 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import BikeStolen from './bikes-stolen';
+  
+// Business Logic
+
+function returnBikes(location) {
+  let promise = BikeStolen.getBikes(location);
+  promise.then(function(bikeData) {
+    printBikes(bikeData);
+  }, function(errorData) {
+    printError(errorData);
+  });
+}
